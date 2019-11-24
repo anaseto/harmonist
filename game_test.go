0c7b15737e3d270fea886e37e4780ef7ef268261
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-11-24 14:17:36 +0000

    add test

--- game_test.go
+++ game_test.go
@@ -6,11 +6,16 @@ func TestInitLevel(t *testing.T) {
 	Testing = true
 	for i := 0; i < 50; i++ {
 		g := &game{}
-		for depth := 0; depth < 11; depth++ {
+		for depth := 0; depth < MaxDepth; depth++ {
 			g.InitLevel()
 			if g.Depth == WinDepth {
 				if g.Dungeon.Cell(g.Places.Shaedra).T != StoryCell {
 					t.Errorf("Shaedra not there: %+v", g.Places.Shaedra)
+				}
+			}
+			if g.Depth == MaxDepth {
+				if g.Dungeon.Cell(g.Places.Artifact).T != StoryCell {
+					t.Errorf("Artifact not there: %+v", g.Places.Artifact)
 				}
 			}
 			for _, m := range g.Monsters {
