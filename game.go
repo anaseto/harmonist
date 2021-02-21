2894ef9e8e0f321dca70e6ca9b6913a902e10d62
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2021-02-21 16:44:54 +0000

    bump dev version

--- game.go
+++ game.go
@@ -10,7 +10,7 @@ import (
 	"github.com/anaseto/gruid/rl"
 )
 
-var Version string = "v0.4-dev-7"
+var Version string = "v0.4-dev-8"
 
 // game contains the game logic's state, without ui stuff. Everything could be
 // in the model struct instead, with only the game logic's fiend exported, as
